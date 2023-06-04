eca937b1b899214540f5f9e90b0b915b84bbd62c
picthon <132414308+picthon@users.noreply.github.com>
2023-06-04 21:50:11 +0300

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -117,7 +117,7 @@ async def generate_session(bot, msg, telethon=False):
     text = "**- كـود تيرمكـس {} ** \n\n{} \n\n**- تم الاستخـراج بواسطـة** Dev: @a_t_9\n**** ".format("تليثـون" if telethon else "بايروجـرام", string_session)
     await client.send_message("me", text)
     await client.disconnect()
-    await phone_code_msg.reply("**- تم إنشاء جلسة تيرمكس {}  بنجاح ..** \n\n**- يرجى التحقق من حافظـة حسابـك واخذ الكود!**\n\n**- Dev: @a_t_9 **\n\n"** ".format("تليثـون" if telethon else "بايروجـرام"))
+    await phone_code_msg.reply("**- كـود تيرمكـس {} ** \n\n{} \n\n**- تم الاستخـراج بواسطـة** Dev: @a_t_9\n******".format("تليثـون" if telethon else "بايروجـرام"))
 
 
 async def cancelled(msg):
